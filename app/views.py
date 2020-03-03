0bf9ff2af9f48f0da6bc1ef5f46a732f18508974
Jason Johnson <jvsonjohnson@gmail.com>
2020-03-02 23:41:38 -0500

    app layout

---
+++ app/views.py
@@ -0,0 +1,50 @@
+from app import app
+from flask import render_template, request, redirect, url_for, flash
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/profiles")
+def profiles():
+    return render_template("profiles.html")
+
+
+@app.route("/<file_name>.txt")
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + ".txt"
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers["X-UA-Compatible"] = "IE=Edge,chrome=1"
+    response.headers["Cache-Control"] = "public, max-age=0"
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template("404.html"), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="8080")

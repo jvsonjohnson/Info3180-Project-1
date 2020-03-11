24784020c5878ea22c525894f1b5c7e25e331b4c
Jason Johnson <jvsonjohnson@gmail.com>
2020-03-11 17:29:01 -0500

    Part 1 Done

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = './app/static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["SECRET_KEY"] = "staywoke"
 app.config[
-    "SQLALCHEMY_DATABASE_URI"] = 'postgresql://teglzpzzkxrkso:5857d0ccdeca10d6c08f2611a24d00ac76054a148ecfa6dc968b5d7b6a36337b@ec2-3-213-192-58.compute-1.amazonaws.com:5432/df29eckben8fnf'
+    "SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:project1@localhost/project1"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)

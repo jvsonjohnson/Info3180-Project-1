e60c616312b4c1becfe1273ba5c24ad04651048f
Jason Johnson <jvsonjohnson@gmail.com>
2020-03-11 15:51:57 -0500

    heroku

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = './app/static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["SECRET_KEY"] = "staywoke"
 app.config[
-    "SQLALCHEMY_DATABASE_URI"] = "postgresql://spjefghktzbkvb:1edae354aab564d6349fcb9f10fc0e39c13997a65c4636ab2a0f6dfcaa0eb015@ec2-184-72-236-57.compute-1.amazonaws.com:5432/d84e0e4finoaoo"
+    "SQLALCHEMY_DATABASE_URI"] = 'postgresql://teglzpzzkxrkso:5857d0ccdeca10d6c08f2611a24d00ac76054a148ecfa6dc968b5d7b6a36337b@ec2-3-213-192-58.compute-1.amazonaws.com:5432/df29eckben8fnf'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)

91048efdef21fdde53d36d7302ef459e3345aa70
Jason Johnson <jvsonjohnson@gmail.com>
2020-04-05 10:51:10 -0500

    heroku app

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,8 @@ if ENV == 'app':
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 else:
     app.debug = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = ""
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"] = "postgres://pmmonraehkxjak:dc4d72d600cbcdf2049172dae34acea86879c49f6149ac8a7d8c4acbb7eee6d3@ec2-54-159-112-44.compute-1.amazonaws.com:5432/dfpgqniavdb5hf"
 db = SQLAlchemy(app)
 
 from app import views  # nopep8

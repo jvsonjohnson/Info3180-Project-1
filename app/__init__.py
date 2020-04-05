1f071346a921726bbf9c1d1008231a68b82addfb
Jason Johnson <jvsonjohnson@gmail.com>
2020-04-05 10:54:07 -0500

    app

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ if ENV == 'app':
 else:
     app.debug = False
     app.config[
-        "SQLALCHEMY_DATABASE_URI"] = "postgres://pmmonraehkxjak:dc4d72d600cbcdf2049172dae34acea86879c49f6149ac8a7d8c4acbb7eee6d3@ec2-54-159-112-44.compute-1.amazonaws.com:5432/dfpgqniavdb5hf"
+        "SQLALCHEMY_DATABASE_URI"] = "postgresql://pmmonraehkxjak:dc4d72d600cbcdf2049172dae34acea86879c49f6149ac8a7d8c4acbb7eee6d3@ec2-54-159-112-44.compute-1.amazonaws.com:5432/dfpgqniavdb5hf"
 db = SQLAlchemy(app)
 
 from app import views  # nopep8

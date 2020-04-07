11074799c58f61a49f2036735fab46d4c26dd6d2
Jason Johnson <jvsonjohnson@gmail.com>
2020-04-07 17:29:00 -0500

    app

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ else:
     app.debug = False
     app.config[
         "SQLALCHEMY_DATABASE_URI"] = "postgresql://pmmonraehkxjak:dc4d72d600cbcdf2049172dae34acea86879c49f6149ac8a7d8c4acbb7eee6d3@ec2-54-159-112-44.compute-1.amazonaws.com:5432/dfpgqniavdb5hf"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
 from app import views  # nopep8
